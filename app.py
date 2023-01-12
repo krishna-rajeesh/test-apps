879c885f897e5fc88fd40e98918f080dc74fcc93
krishna rajeesh <65469475+krishna-rajeesh@users.noreply.github.com>
2023-01-12 16:48:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
     """Say hello"""
-    return 'Hello, World!'
+    return 'Hello, Datadog!'
 
 
 
